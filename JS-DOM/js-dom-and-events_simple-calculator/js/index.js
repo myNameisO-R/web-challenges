d79fa993dc87b6079e7fcaff3e4700cc6d23f139
Orna <kisinnr1@gmail.com>
2024-11-10 17:15:18 +0100

    Add solutions

--- JS-DOM/js-dom-and-events_simple-calculator/js/index.js
+++ JS-DOM/js-dom-and-events_simple-calculator/js/index.js
@@ -15,29 +15,40 @@ const moduloButton = document.querySelector('[data-js="modulo"]');
 
 addButton.addEventListener("click", () => {
   // 1. Add the two operands and store the result in a variable. Log the variable's value to the console.
+  const result = operand1 + operand2;
+  console.log(result);
 });
-
-add
-
 
 subtractButton.addEventListener("click", () => {
   // 2. Subtract the two operands and store the result in a variable. Log the variable's value to the console.
+
+  const result = operand1 - operand2;
+  console.log(result);
 });
 
 multiplyButton.addEventListener("click", () => {
   // 3. Multiply the two operands and store the result in a variable. Log the variable's value to the console.
+  const result = operand1 * operand2;
+  console.log(result);
 });
 
 divideButton.addEventListener("click", () => {
   // 4. Divide the two operands and store the result in a variable. Log the variable's value to the console.
+
+  const result = operand1 / operand2;
+  console.log(result);
 });
 
 exponentButton.addEventListener("click", () => {
   // 5. Raise "operand1" to the value of "operand2" and store the result in a variable. Log the variable's value to the console.
+  const result = operand1 ** operand2;
+  console.log(result);
 });
 
 moduloButton.addEventListener("click", () => {
   // 6. Calculate the remainder when dividing "operand1" by "operand2" and store the result in a variable. Log the variable's value to the console.
+  const remainder = operand1 % operand2;
+  console.log(remainder);
 });
 
 /*
@@ -64,6 +75,7 @@ const divideByTwoButton = document.querySelector('[data-js="divide-by-two"]');
 
 increaseByOneButton.addEventListener("click", () => {
   // 7. Increase the value of "operand1" by 1. Log the new value to the console.
+  console.log(operand1 + 1);
 });
 
 increaseByFiveButton.addEventListener("click", () => {
