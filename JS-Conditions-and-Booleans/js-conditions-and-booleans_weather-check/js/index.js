54fd27dda8c979c91e870b7575367d888b96de41
Orna <kisinnr1@gmail.com>
2024-11-10 20:58:08 +0100

    Solve second part

--- JS-Conditions-and-Booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ JS-Conditions-and-Booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,5 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy";
+const weather = "snowy";
 
 switch (weather) {
   case "sunny":
@@ -16,4 +16,20 @@ switch (weather) {
 }
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22;
+const temperature = 10;
+
+switch (weather) {
+  case "sunny":
+    console.log(
+      "It's sunny" + (temperature > 20 ? "and warm" : ", but chilly") + "!"
+    );
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    console.log(temperature < 0 ? "Freezing snow!" : "It's snowing, stay warm");
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
+}
