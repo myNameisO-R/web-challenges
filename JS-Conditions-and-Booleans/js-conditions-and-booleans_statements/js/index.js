d43647cc6f7fe6ec59614ea14add5c653296f146
Orna <kisinnr1@gmail.com>
2024-11-10 19:13:33 +0100

    Solve first part of the statements challenge

---
+++ JS-Conditions-and-Booleans/js-conditions-and-booleans_statements/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+
+const receivedPassword = "password1234";
+
+console.log(
+  receivedPassword === SUPER_SECRET_PASSWORD ? "Welcome!" : "Access denied!"
+);
+
+// Part 2: Even / Odd
+const number = 6;
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = "";
+
+console.log(statement);
+
+// Part 5: Greeting
+const userName = "Archibald";
+
+const greeting = "Hello " + "//enter your code here" + "!";
+
+console.log(greeting);
