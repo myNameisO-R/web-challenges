be41a284687c44c39a63059fc1bf4dda3a0248dd
Orna <kisinnr1@gmail.com>
2024-11-12 15:16:13 +0100

    first challenge

--- JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,8 @@
 console.clear();
+
+const firstInput = document.querySelector('[data-js="firstInput"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
