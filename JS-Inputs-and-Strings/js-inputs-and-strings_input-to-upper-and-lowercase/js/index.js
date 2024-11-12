76ac6e37a3a8f5284c29b92348f4060c864c7736
Orna <kisinnr1@gmail.com>
2024-11-12 15:39:19 +0100

    Add Lowercase

--- JS-Inputs-and-Strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ JS-Inputs-and-Strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -6,3 +6,10 @@ const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
 });
+
+const input = document.querySelector('[data-js="first-input"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+
+uppercaseButton.addEventListener("click", () => {
+  input.value = input.value.toLowerCase();
+});
