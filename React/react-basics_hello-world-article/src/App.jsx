d084965530fb51709aa5ba30f6197eccf7877d81
Orna <kisinnr1@gmail.com>
2024-11-25 12:00:05 +0100

    Add components to the function

--- React/react-basics_hello-world-article/src/App.jsx
+++ React/react-basics_hello-world-article/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return <div>Replace me with your Component!</div>;
+export default function HelloWorldArticle() {
+  return (
+    <div>
+      <h1>HelloWorldArticle</h1>
+      <p>have a nice day.</p>
+    </div>
+  );
 }
