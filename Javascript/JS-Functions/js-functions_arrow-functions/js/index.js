6080d7fbb61e326a0b06c74704a74440af1b7ae6
Orna <kisinnr1@gmail.com>
2024-12-01 15:32:51 +0100

    Add functions

--- Javascript/JS-Functions/js-functions_arrow-functions/js/index.js
+++ Javascript/JS-Functions/js-functions_arrow-functions/js/index.js
@@ -19,6 +19,22 @@ function getCurrentHour() {
   }
 }
 
+const getCurrentHour = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+};
+
+console.log("Current hour: ", getCurrentHour());
+
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
@@ -27,11 +43,17 @@ function cleanInput(string) {
   return string.toLowerCase().trim();
 }
 
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+
+console.log("Vector length:" + getVectorLength(1, 2, 3));
+
+const cleanInput = (string) => string.toLowerCase().trim();
+
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +63,17 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
+
+console.log("isOddAndsmall:", isOddAndSmall(10));
 
 const add3 = (a, b, c) => a + b + c;
 
 const repeat10 = (string) => string.repeat(10);
+
+function add3(a, b, c) {
+  const sum = a + b + c;
+  return sum;
+}
+const sum = add3(2, 3, 4);
+console.log(sum);
