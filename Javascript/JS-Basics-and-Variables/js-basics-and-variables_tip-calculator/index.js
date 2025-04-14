f3f82028c7ef8ecdd34cefbf0ca7592a08fe1ff9
Orna <kisinnr1@gmail.com>
2025-04-14 09:26:43 +0200

    add calculator

--- Javascript/JS-Basics-and-Variables/js-basics-and-variables_tip-calculator/index.js
+++ Javascript/JS-Basics-and-Variables/js-basics-and-variables_tip-calculator/index.js
@@ -6,11 +6,19 @@ based on the cost of the meal and the percentage of the tip.
 
 1. Define two variables:
    - A variable that stores the value of the meal cost.
+   let mealCost = 0 ;
    - A variable that stores the value of the tip percentage.
-
+   let tipPrecantage = mealCost + 10%;
 2. Calculate the tip amount by multiplying the meal cost by the tip percentage, and store the result in a variable called `tipAmount`.
+let tipAmount = mealCost * tip Percentage;
 
 3. Calculate the total cost of the meal, including tip, by adding `mealCost` and `tipAmount`, and store the result in a variable called `totalCost`.
-
+let totalCost = mealCost + tipAmount;
 4. Log a message to the console that includes the meal cost, tip amount, and total cost.
 */
+
+let mealCost = 100;
+let tipPrecantage = 20;
+let tipAmount = (mealCost * tipPrecantage) / 100;
+let totalCost = mealCost + tipAmount;
+console.log(totalCost);
