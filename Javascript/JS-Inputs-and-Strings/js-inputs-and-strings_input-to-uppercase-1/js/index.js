04ca60993ac73fdd43d75479f91db516b308a1b4
Orna <kisinnr1@gmail.com>
2025-05-04 14:43:38 +0200

    add uppercase

--- Javascript/JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase-1/js/index.js
+++ Javascript/JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase-1/js/index.js
@@ -1,6 +1,7 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+
 const button = document.querySelector('[data-js="button-uppercase"]');
 
 button.addEventListener("click", () => {
